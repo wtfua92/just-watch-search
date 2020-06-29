39c827053a39ed7b5de9163b19fa02d509879bbc
Andrii Tynok <atynok@homeaway.com>
2020-06-29 13:34:31 +0300

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <SearchBar msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    SearchBar
   }
 });
 </script>
@@ -22,8 +21,5 @@ export default Vue.extend({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label for="search-bar">Search:</label>
+        <input id="search-bar" type="text">
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import debounce from ""
+
+export default Vue.extend({
+  name: "SearchBar",
+  props: {
+    msg: String
+  },
+    methods: {
+
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

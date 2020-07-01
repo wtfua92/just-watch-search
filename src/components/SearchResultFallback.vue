08d5484bc634fcfdf3bf481b91ad048be2d6198a
Andrii Tynok <atynok@homeaway.com>
2020-07-01 07:16:11 +0300

    Added loading spinner

---
+++ src/components/SearchResultFallback.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search-result search-result--fallback">
+    Find your show
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SearchResultFallback"
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
